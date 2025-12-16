e75f4ca12e82ddec956f81fd3a422336ba0f04d0
EskilE <eskilme08@gmail.com>
2025-12-16 11:24:42 +0100

    Testing with timers for the timer part of the app, DispatcherTimer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,40 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Local_Study_and_Focus_Companion
 {
     public partial class MainWindow : Window
-    {       
-        int num = 0;
+    {
+        DispatcherTimer timer = new DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
         }
+
+        int seconds = 0;
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            counter.Content = seconds.ToString();
+        }
+
 
         public void StartTimer(object sender, RoutedEventArgs e)
         {
-                       
+            timer.Start();
         }
+
+        public void StopTimer(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        // leggg til en reset button for timeren 
     }
 }
